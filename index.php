3f8125e615d7176ed6fd1f37901f73720ce6a4fd
duy123prodn <51693250+duy123prodn@users.noreply.github.com>
2019-06-11 17:00:46 +0700

    Update index.php
    Add Hello World
    Link to vnexpress and link to 24h

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
   <title>Database Test</title>
  </head>
  <body>
+ <?php echo '<p>Hello World</p>'; 
+ 
+ ?>
  <?php echo '<p>Hello This is first cloud app</p>'; 
  
  ?> 
+     <a href="vnexpress.net" target="_blank" >Link to vnexpress</a>
+     <br/>
+     <a href="24h.com.vn" target="_blank" >Link to 24h</a>
+     <br/>
      <a href="ConnectToDB.php" target="_blank" >Connect database</a>
      <br/>
      <a href="InsertData.php" target="_blank" >Insert data to the database</a>
