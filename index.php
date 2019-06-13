c5caaea1ea9d085f73c7493a3b0312b1dc77ef55
duy123prodn <51693250+duy123prodn@users.noreply.github.com>
2019-06-13 19:27:07 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -5,9 +5,16 @@
   ?>
   <?php echo '<h2 style="color: blue; text-align: center">Thank you for coming here ! </h2>'
   ?>
-  <?php echo '<h3> <a href="https://www.facebook.com/duy123prodn" target="_blank"> ==> LINK TO MY FACEBOOK <== </a></h3>'
+  <?php echo '<h3 style="color: green; text-align: center"> <a href="https://www.facebook.com/duy123prodn" target="_blank"> ==> LINK TO MY FACEBOOK <== </a></h3>'
   ?>
  </head>
  <body>
+    <ul>
+        <li>SOME MY INFORMATIONS</li>
+        <li>Full Name: Ho Nhat Duy.</li>
+        <li>Date of Birth: 27/10/1999.</li>
+        <li>Ages: 20 years old.</li>
+        <li>Hometown: Da Nang City</li>
+    </ul>
  </body>
 </html>
