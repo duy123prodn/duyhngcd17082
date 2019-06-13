7b277a805875d31383c6d3096e0b84a1b5d34d28
duy123prodn <51693250+duy123prodn@users.noreply.github.com>
2019-06-13 17:29:13 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -6,38 +6,26 @@
   <?php echo '<h2 style="color: blue; text-align: center">Thank you for coming here ! </h2>'
   ?>
  </head>
-  <style>
-    .picture {
-        float: left;
-        border-style: : solid;
-        border-width: 1px;
-        border-color: silver;
-     }
-
-    .clickdata {
-        float: left;
-        margin-left: 20px;
-        margin-top: 200px;
-     }
-    .clickdata input[type="submit"] {
-        font-size: 0.9em;
-        color: #fff;
-        background-color: #403b37;
-        border: none;
-        padding: 2px;
-        width: 200%;
-        outline: none;
-        text-align: center;
-    }
-    .clickdata input[type="submit"]:hover {
-        background: #FF00CC;
-    }
-    </style>
  <body>
-    <div class=title style="width: 280px; height: 25px; background: pink; margin-top: 100px; margin-left: 500px ">
+    <div class=title style="width: 280px; height: 25px; background: pink; margin-top: 100px; margin-left: 1000px ">
         <ul>
             <li>
                 <span style="color: white"> <a href="https://www.facebook.com/duy123prodn" target="_blank" >Link to my facebook</a> </span>
+                <a href="https://vnexpress.net/" target="_blank" >Đọc báo trên VnExpress Thì Nhấn Vào Đây</a>
+                 <br/>
+                <a href="https://www.24h.com.vn/" target="_blank" >Đọc báo ở trang 24h thì nhấn vào đây</a>
+                <br/>
+     
+        <a href="ConnectToDB.php" target="_blank" >Connect to Database: CLICK HERE</a>
+        <br/>
+    
+        <a href="InsertData.php" target="_blank" >Provide me your Information. Thank you <3 </a>
+        <br/>
+     
+     <a href="UpdateData.php" target="_blank" >Update Data</a>
+                <br/>
+   
+     <a href="DeleteData.php" target="_blank" >Delete Data</a> 
             </li>
         </ul>
     </div >
@@ -47,19 +35,18 @@
 <div class=clickdata>
      <a href="https://vnexpress.net/" target="_blank" >Đọc báo trên VnExpress Thì Nhấn Vào Đây</a>
      <br/>
-     <p>===============================================</p>
      <a href="https://www.24h.com.vn/" target="_blank" >Đọc báo ở trang 24h thì nhấn vào đây</a>
      <br/>
-     <p>===============================================</p>
+     
      <a href="ConnectToDB.php" target="_blank" >Connect to Database: CLICK HERE</a>
      <br/>
-     <p>===============================================</p>
+    
      <a href="InsertData.php" target="_blank" >Provide me your Information. Thank you <3 </a>
      <br/>
-     <p>===============================================</p>
+     
      <a href="UpdateData.php" target="_blank" >Update Data</a>
      <br/>
-     <p>===============================================</p>
+   
      <a href="DeleteData.php" target="_blank" >Delete Data</a> 
 </div>
  </body>
