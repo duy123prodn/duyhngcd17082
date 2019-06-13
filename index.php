504d2d31c26c83096e6232bf403553e9042bf91e
duy123prodn <51693250+duy123prodn@users.noreply.github.com>
2019-06-13 15:12:15 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <html>
  <head>
-  <title>Database Test</title>
+  <title>Duy's Simple Website</title>
+  
  </head>
+
  <body>
- <?php echo '<p>Hello World</p>'; 
- 
- ?>
- <?php echo '<p>Hello This is first cloud app</p>'; 
- 
- ?> 
+    <div class=title>
+        <ul>
+            <li>
+                <p><span>H<span/>ello !!! WELCOME TO MY WEBSITE<p>
+            </li>
+        </ul>
+    </div class=>
+    <div>
+        
+    </div>
+
      <a href="https://vnexpress.net/" target="_blank" >Link to vnexpress</a>
      <br/>
      <a href="https://www.24h.com.vn/" target="_blank" >Link to 24h</a>
@@ -18,8 +25,8 @@
      <a href="InsertData.php" target="_blank" >Insert data to the database</a>
      <br/>
      <a href="UpdateData.php" target="_blank" >Update data to the database</a> 
-    <br/>
-    <a href="DeleteData.php" target="_blank" >Delete data to the database</a> 
+     <br/>
+     <a href="DeleteData.php" target="_blank" >Delete data to the database</a> 
 
  </body>
 </html>
