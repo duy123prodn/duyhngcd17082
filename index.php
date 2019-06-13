f4f70aa50dfd083cf3d910a3d34a40ebeb0dcc93
duy123prodn <51693250+duy123prodn@users.noreply.github.com>
2019-06-13 19:06:15 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
   ?>
  </head>
  <body>
-    <div class=box123 style="width: 1300px">
-        <div class=box1 style="width: 400px; margin-left: 1px; margin-top: 1px; height: 200px;">
+    <div class=box123 style="width: 1300px; background: blue;">
+        <div class=box1 style="width: 350px; margin-left: 1px; margin-top: 1px; height: 200px; background: red;">
             <ul>
             <li>    
                 <a href="https://www.facebook.com/duy123prodn" target="_blank" >LINK TO MY FACEBOOK</a>
@@ -22,7 +22,7 @@
             </li>
         </ul>
         </div>
-        <div class=box2 style="width: 400px; margin-left: 449px; margin-top: 1px; height: 200px;">
+        <div class=box2 style="width: 350px; margin-left: 449px; margin-top: 1px; height: 200px; background: yellow;">
             <ul>
             <li>    
                 <a href="https://www.facebook.com/duy123prodn" target="_blank" >LINK TO MY FACEBOOK</a>
@@ -36,7 +36,7 @@
             </li>
         </ul>
         </div>
-        <div class=box3 style="width: 400px; margin-right: 1px;margin-top: 1px; height: 200px;">
+        <div class=box3 style="width: 350px; margin-right: 1px;margin-top: 1px; height: 200px; background: pink;">
             <ul>
             <li>    
                 <a href="https://www.facebook.com/duy123prodn" target="_blank" >LINK TO MY FACEBOOK</a>
