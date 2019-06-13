a93a7a822a3d6fc9a39b882d9baf7c48007f9b94
duy123prodn <51693250+duy123prodn@users.noreply.github.com>
2019-06-13 19:20:28 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -5,51 +5,9 @@
   ?>
   <?php echo '<h2 style="color: blue; text-align: center">Thank you for coming here ! </h2>'
   ?>
+  <?php echo '<h3> <a href="https://www.facebook.com/duy123prodn" target="_blank"> ==> LINK TO MY FACEBOOK <== </a></h3>'
+  ?>
  </head>
  <body>
-    <div class=box123 style="width: 1300px; background: blue; height: 300px; text-align: center;">
-        <div class=box1 style="width: 350px; margin-left: 1px; margin-top: 1px; height: 200px; background: red;">
-            <ul>
-            <li>    
-                <a href="https://www.facebook.com/duy123prodn" target="_blank" >LINK TO MY FACEBOOK</a>
-            </li>
-
-            <li>
-                <a href="https://vnexpress.net/" target="_blank" >VNEXPRESS</a>
-            </li>   
-            <li>
-                <a href="https://www.24h.com.vn/" target="_blank" >24h.com.vn</a>
-            </li>
-        </ul>
-        </div>
-        <div class=box2 style="width: 350px; margin-left: 449px; margin-top: 1px; height: 200px; background: yellow;">
-            <ul>
-            <li>    
-                <a href="https://www.facebook.com/duy123prodn" target="_blank" >LINK TO MY FACEBOOK</a>
-            </li>
-
-            <li>
-                <a href="https://vnexpress.net/" target="_blank" >VNEXPRESS</a>
-            </li>   
-            <li>
-                <a href="https://www.24h.com.vn/" target="_blank" >24h.com.vn</a>
-            </li>
-        </ul>
-        </div>
-        <div class=box3 style="width: 350px; margin-right: 1px;margin-top: 1px; height: 200px; background: pink;">
-            <ul>
-            <li>    
-                <a href="https://www.facebook.com/duy123prodn" target="_blank" >LINK TO MY FACEBOOK</a>
-            </li>
-
-            <li>
-                <a href="https://vnexpress.net/" target="_blank" >VNEXPRESS</a>
-            </li>   
-            <li>
-                <a href="https://www.24h.com.vn/" target="_blank" >24h.com.vn</a>
-            </li>
-        </ul>
-        </div>
-    </div>
  </body>
 </html>
