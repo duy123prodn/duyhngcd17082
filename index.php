a419b297d9ec823f0a857f4bb7eb51c5afce8aab
duy123prodn <51693250+duy123prodn@users.noreply.github.com>
2019-06-13 20:05:37 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -7,14 +7,16 @@
   ?>
   <?php echo '<h3 style="color: green; text-align: center"> <a href="https://www.facebook.com/duy123prodn" target="_blank"> ==> LINK TO MY FACEBOOK <== </a></h3>'
   ?>
+  
  </head>
+ 
  <body>
     <h2><span>S</span>ome Informations about me:</h2>
     <div class=SumBOX>
-        <div calss=pictute style="width: 250px; margin-left: 50px; height: 500px; border-color: black; border: dashed; ">
-            <img src="images/duy.jpg">
+        <div class=pictute style="width: 250px; margin-left: 50px; height: 300px; border-color: black; border: dashed; ">
+            <img src="../images/duy.jpg" style="width: 250px; margin-left: 50px; height: 300px">
         </div>
-        <div>
+        <div class=infor style="margin-left: 50px;">
             <p>Full Name: Ho Nhat Duy. </p>
             <p>Date of Birth: 27/10/1999. </p>
             <p>Ages: 20 years old. </p>
