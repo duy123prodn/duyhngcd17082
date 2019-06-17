4bb7f7984d3eced66bb78f5ff61b988ead6987fb
duy123prodn <51693250+duy123prodn@users.noreply.github.com>
2019-06-17 23:33:49 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -19,32 +19,33 @@
  
  <body>
     <h2><span>S</span>ome Informations about me:</h2>
-            <div id="content" class="container" style="width: 900px; border: 1px solid #333; padding: 10px;">
+    <div id="content0" class="container0" style="width: 900px; border: 1px solid #333; padding: 10px;">
+                <div id="content" class="container" style="width: 550px; border: 1px solid #333; padding: 10px;">
             
-            <div id="pictute" style="width: 240px; height: 300px; border-color: black; border: dashed; float:left;">
+                    <div id="pictute" style="width: 240px; height: 300px; border-color: black; border: dashed; float:left;">
 
-                <img src="../images/duy.jpg" style="width: 240px; height: 300px;"> 
+                        <img src="../images/duy.jpg" style="width: 240px; height: 300px;"> 
 
-            </div>
+                    </div>
 
-            <div id="infor" style="margin-left: 30px; width: 240px; height: 300px; float: right; padding: 1px; margin-right: 10px;">
-                <div style="padding: 1px;">
-                <p>Full Name: Ho Nhat Duy. </p>
-                <p>Date of Birth: 27/10/1999. </p>
-                <p>Ages: 20 years old. </p>
-                <p>Hometown: Da Nang City </p>
-                <p>Height: 173cm </p>
-            <p>Weight: 62kg </p>
-                <p>Hobbies: Hanging out, playing games, sleeping, traveling..</p>
+                    <div id="infor" style="margin-left: 30px; width: 240px; height: 300px; float: right; padding: 1px; margin-right: 10px;">
+                        <div style="padding: 1px;">
+                        <p>Full Name: Ho Nhat Duy. </p>
+                        <p>Date of Birth: 27/10/1999. </p>
+                        <p>Ages: 20 years old. </p>
+                        <p>Hometown: Da Nang City </p>
+                        <p>Height: 173cm </p>
+                        <p>Weight: 62kg </p>
+                        <p>Hobbies: Hanging out, playing games, sleeping, traveling..</p>
+                    </div>
                 </div>
-            </div>
-            <dir class="clear" style=" clear: both;"></dir>
+                <dir class="clear" style=" clear: both;"></dir>
 
+            </div>
             <div id="data" style="width: 240px; height: 300px; border-color: black; border: solid #333 ; float:left;">
-
-            
-
             </div>
-        </div>
+         <dir class="clear" style=" clear: both;"></dir>
+    </div>
+            
  </body>
 </html>
