1b33bbce1d0662a0766e655f8b1309e1031fad9f
duy123prodn <51693250+duy123prodn@users.noreply.github.com>
2019-06-24 04:43:41 +0700

    Add files via upload

--- UpdateData.php
+++ UpdateData.php
@@ -29,10 +29,10 @@ s
 
 $id = $_GET['stuid'];
 
-$sql = 'SELECT * FROM student WHERE stuid=:StudentID ';
+$sql = 'SELECT * FROM student WHERE stuid=:stuid ';
 
 $stmt = $pdo->prepare($sql);
-$stmt->execute([':StudentID' => $id ]);
+$stmt->execute([':stuid' => $id ]);
 
 $person = $stmt->fetch(PDO::FETCH_OBJ);
 
