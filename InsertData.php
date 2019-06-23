bb8ff0c5d789a2a519390a6dab2f489abdfdfb21
duy123prodn <51693250+duy123prodn@users.noreply.github.com>
2019-06-24 02:46:27 +0700

    Add files via upload

--- InsertData.php
+++ InsertData.php
@@ -1,22 +1,25 @@
+<?php require 'header.php'; ?>
+<h1>DATABASE CONNECTION</h1>
+
 <?php
+
 if (empty(getenv("DATABASE_URL"))){
     echo '<p>The DB does not exist</p>';
-    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
+    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=duyhngcd17082', 'postgres', '123456');
 }  else {
      
    $db = parse_url(getenv("DATABASE_URL"));
    $pdo = new PDO("pgsql:" . sprintf(
-        "host=ec2-174-129-240-67.compute-1.amazonaws.com;port=5432;user=wrflrxtavasvqh;password=fbfef36049fbd28f1200e3a775a389e014838e86522765e67782f9cf7a3f516b;dbname=d3mmhribgmc6bf",
+        "host=ec2-54-225-72-238.compute-1.amazonaws.com;port=5432;user=xajjrlkdrlahpp;password=b5cd985b527ee9c10e0b2ad61c6e659276f46ee188a13b97bd8bbfe186c6800f;dbname=dalogq746t98vp",
         $db["host"],
         $db["port"],
         $db["user"],
         $db["pass"],
         ltrim($db["path"], "/")
    ));
+
+
 }  
-if($pdo === false){
-     echo "ERROR: Could not connect Database";
-}
 
 $message = '';
 if (isset($_POST['StudentID']) && isset($_POST['fname'])  && isset($_POST['email']) && isset($_POST['classname']) ) {
@@ -24,6 +27,7 @@ if (isset($_POST['StudentID']) && isset($_POST['fname'])  && isset($_POST['email
   $fname = $_POST['fname'];
   $email = $_POST['email'];
   $classname = $_POST['classname'];
+  
   $sql = 'INSERT INTO student(stuid, fname, email, classname) VALUES(:StudentID, :fname, :email, :classname)';
   $stmt = $pdo->prepare($sql);
   if ($stmt->execute([':StudentID' => $stuid, ':fname' => $fname, ':email' => $email, ':classname' => $classname]) == TRUE) {
