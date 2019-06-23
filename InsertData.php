1d58826e3b3618df6c323776a8591356f439e4a3
duy123prodn <51693250+duy123prodn@users.noreply.github.com>
2019-06-24 02:54:31 +0700

    Add files via upload

--- InsertData.php
+++ InsertData.php
@@ -37,5 +37,26 @@
   </div>
 </div>
 
+<?php
+
+if (empty(getenv("DATABASE_URL"))){
+    echo '<p>The DB does not exist</p>';
+    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=duyhngcd17082', 'postgres', '123456');
+}  else {
+     
+   $db = parse_url(getenv("DATABASE_URL"));
+   $pdo = new PDO("pgsql:" . sprintf(
+        "host=ec2-54-225-72-238.compute-1.amazonaws.com;port=5432;user=xajjrlkdrlahpp;password=b5cd985b527ee9c10e0b2ad61c6e659276f46ee188a13b97bd8bbfe186c6800f;dbname=dalogq746t98vp",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+   ));
+}  
+if($pdo === false){
+     echo "ERROR: Could not connect Database";
+?>
+
 
 
