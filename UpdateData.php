e0eabfbf54f053781d9402dd6fb10378a1adcdc0
duy123prodn <51693250+duy123prodn@users.noreply.github.com>
2019-06-24 04:31:10 +0700

    Add files via upload

--- UpdateData.php
+++ UpdateData.php
@@ -29,27 +29,14 @@ s
 
 $id = $_GET['stuid'];
 
-$sql = 'SELECT * FROM student WHERE stuid=:stuid';
+$sql = 'SELECT * FROM student WHERE stuid=:StudentID ';
 
 $stmt = $pdo->prepare($sql);
-$stmt->execute([':stuid' => $id ]);
+$stmt->execute([':StudentID' => $id ]);
 
 $person = $stmt->fetch(PDO::FETCH_OBJ);
 
-if (isset($_POST['StudentID']) && isset($_POST['fname'])  && isset($_POST['email']) && isset($_POST['classname']) )
-{
-  $stuid = $_POST['StudentID'];
-  $fname = $_POST['fname'];
-  $email = $_POST['email'];
-  $classname = $_POST['classname'];
 
-  $sql = 'UPDATE student SET stuid=:StudentID, fname=:fname, email=:email, classname=:classname WHERE stuid=:StudentID';
-  $stmt = $pdo->prepare($sql);
-  if ($stmt->execute([':StudentID' => $stuid, ':fname' => $fname, ':email' => $email, ':classname' => $classname]) )
-  {
-    header("Location: ConnectToDB.php");
-  }
-}
 ?>
 
 
